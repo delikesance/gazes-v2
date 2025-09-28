de7df002c02ec9c76201ef8e2117d1aa0b357cf7
garder500 <jeremy27.clara22@gmail.com>
2025-09-29 00:17:07 +0900

    Changement de la configuration pour utiliser 'node-server' au lieu de 'vercel' et mise à jour des scripts de démarrage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -97,7 +97,7 @@ export default defineNuxtConfig({
   },
 
    nitro: {
-     preset: 'vercel',
+     preset: 'node-server',
      compressPublicAssets: true,
      prerender: {
        routes: ['/']
